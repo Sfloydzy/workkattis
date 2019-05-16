e6894de867cdb5fdf477700b624a858cf0cce6b5
Sfloydzy <e0310657@u.nus.edu>
2019-05-16 16:33:42 +0800

    Progress Made 	modified:   CalculatingDartScores.cpp 	new file:   ps1.php

--- CalculatingDartScores.cpp
+++ CalculatingDartScores.cpp
@@ -11,6 +11,9 @@ int n;
 vi dist;
 vd AL;
 
+vds match;
+
+
 void bfs () {
   dist[0] = 0;
   queue<int> score;
@@ -22,15 +25,31 @@ void bfs () {
     for (auto &i : AL) {
       int p = s + i.first * i.second;
       if (p <= n) {
-        score.push(p);
+        if (dist[p] > dist[s] + 1 && (dist[s] + 1) <= 3) {
+          score.push(p);
+          dist[p] = dist[s] + 1;
+          string mx;
+          if (i.first == 1)
+            mx = "single";
+
+          else if (i.first == 2)
+            mx = "double";
+
+          else
+            mx = "triple";
+
+          match[dist[p] - 1] = make_pair(mx, i.second);
+        }
       }
     }
   }
 }
+
 int main () {
   ios::sync_with_stdio(false); cin.tie(NULL);
   cin >> n;
   dist.assign(180, 1e9);
+  match.assign(3, pair<string, int> ());
   for (int i = 1; i <= 20; ++i) {
     for (int j = 1; j <= 3; ++j) {
       AL.push_back(make_pair(j,i));
@@ -38,7 +57,9 @@ int main () {
   }
 
   bfs();
-
+  for (auto &i : match) {
+    cout << i.first << " " << i.second << endl;
+  }
 
   return 0;
 }
---
+++ ps1.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title> <?php echo "PHP Object Oriented Programming";?></title>
+</head>
+
+<body>
+  <h1><?php echo "Stuff Here";?></h1>
+</body>
+</html>
